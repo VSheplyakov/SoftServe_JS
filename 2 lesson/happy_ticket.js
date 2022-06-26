db6039caac7dfbe4e6aa90c7f392c1318416e80f
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-06-26 22:32:32 +0300

    edited task 1

--- 2 lesson/happy_ticket.js
+++ 2 lesson/happy_ticket.js
@@ -1,38 +1,41 @@
 /**
  * 1. Щасливий квиток
- * Напишіть веб-аплікацію для розрахунку щасливого квитка. Користувач вводить шестизначний номер квитка (трамвай/тролейбус).
- * Визначте, чи дорівнює сума перших трьох цифр сумі останніх трьох цифр. Виведіть повідомлення про "щасливість" квитка користувачеві.
+ * Напишіть веб-аплікацію для розрахунку щасливого квитка.
+ * Користувач вводить шестизначний номер квитка (трамвай/тролейбус).
+ * Визначте, чи дорівнює сума перших трьох цифр сумі останніх трьох цифр.
+ * Виведіть повідомлення про "щасливість" квитка користувачеві.
  */
 
-//let num = prompt('Enter ticket number:');
-let num = 111111; //prompt('Enter ticket number:');
+let num = prompt('Enter ticket number:');
 
-function check(num) {
-  let numStr = num.toString();
-  console.log(numStr);
+const correctInput = (num) => {
+  isNaN(num) || num.toString().length !== 6
+    ? alert(`It's not valid number`)
+    : num;
+};
+correctInput(num);
+
+let ticketNum = num.toString();
+
+function check(ticketNum) {
   const partFirst = () => {
     let countFirst = 0;
     for (let i = 0; i <= 2; i++) {
-      countFirst += Number(numStr[i]);
-      console.log(countFirst);
+      countFirst += Number(ticketNum[i]);
     }
     return countFirst;
   };
 
   const partSecond = () => {
     let countSecond = 0;
-    for (let i = Math.ceil(numStr.lenght / 2); i <= numStr.lenght - 1; i++) {
-      countSecond += Number(numStr[i]);
-      console.log(countSecond);
+    for (let i = 3; i <= 5; i++) {
+      countSecond += Number(ticketNum[i]);
     }
     return countSecond;
   };
 
   partFirst() !== partSecond()
-    ? console.log('not') //alert(`It's not happy ticket :(`)
-    : console.log('yes'); //alert(`You have HAPPY TICKET - Eat it! :D`);
+    ? alert(`It's not happy ticket :(`)
+    : alert(`You have HAPPY TICKET - Eat it! :D`);
 }
-check(num);
-// let numStr = num.toString();
-// let i = numStr.length/2;
-// alert(i)
+check(ticketNum);
