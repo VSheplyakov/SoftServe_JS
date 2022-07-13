4537cb112ddca9b0ee5884ff7fabd95351e01ac4
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-07-13 13:56:53 +0300

    add 4.sotr

--- 4 lesson/sort_string.js
+++ 4 lesson/sort_string.js
@@ -6,28 +6,28 @@
  * "4of Fo1r pe6ople g3ood th5e the2" --> "Fo1r the2 g3ood 4of th5e pe6ople"
  * "" --> "" *
  */
-let str = 'is2 Thi1s T4est 3a';
+let str = '4of Fo1r pe6ople g3ood th5e the2';
 
-// function sortStr(str) {
-//   arr = str.split(' ');
-//   console.log(arr);
-//   const result = [];
+function sortStr(str) {
+  arr = str.split(' ');
 
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i].includes('1')) result[0] = arr[i];
-//     if (arr[i].includes('2')) result[1] = arr[i];
-//     if (arr[i].includes('3')) result[2] = arr[i];
-//     if (arr[i].includes('4')) result[3] = arr[i];
-//     if (arr[i].includes('5')) result[4] = arr[i];
-//     if (arr[i].includes('6')) result[5] = arr[i];
-//     if (arr[i].includes('7')) result[6] = arr[i];
-//     if (arr[i].includes('8')) result[7] = arr[i];
-//     if (arr[i].includes('9')) result[8] = arr[i];
-//   }
-//   console.log(result);
-// }
-// sortStr(str);
+  const result = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(1)) result[0] = arr[i];
+    if (arr[i].includes(2)) result[1] = arr[i];
+    if (arr[i].includes(3)) result[2] = arr[i];
+    if (arr[i].includes(4)) result[3] = arr[i];
+    if (arr[i].includes(5)) result[4] = arr[i];
+    if (arr[i].includes(6)) result[5] = arr[i];
+    if (arr[i].includes(7)) result[6] = arr[i];
+    if (arr[i].includes(8)) result[7] = arr[i];
+    if (arr[i].includes(9)) result[8] = arr[i];
+  }
+  return console.log(result.join(' '));
+  
+}
+sortStr(str);
 
 // function sortStr(str) {
 //   arr = str.split(' ');
@@ -45,17 +45,20 @@ let str = 'is2 Thi1s T4est 3a';
 // }
 // console.log(sortStr(str));
 
+// function sortStr(str) {
+//   arr = str.split(' ');
+//   console.log(arr);
+//   const result = [];
+//   for ( let word of arr) {
+//     //console.log(word)
+//     //console.log(typeof word)
+//     // console.log(word.includes(1))
+//     for (let i = 0; i >= arr.length; i++)
+//     if (word.includes(i)) {
+//       result = result.splice[i]
 
+//     }
+//     //console.log(result)
+//   }
 
-function sortStr(str) {
-  arr = str.split(' ');
-  console.log(arr);
-  const result = [];
-  
-  
-    
-
-    
-  
-}
-console.log(sortStr(str));
+// }
