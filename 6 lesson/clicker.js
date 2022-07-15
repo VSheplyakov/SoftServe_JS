8193732a92208b6a9e029b9c33dc3bdd3cf5e2b0
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-07-15 16:38:48 +0300

    add clicker

--- 6 lesson/clicker.js
+++ 6 lesson/clicker.js
@@ -5,3 +5,21 @@
 При кліку на кнопку "Відняти" рахунок віднімається на 1
 
  */
+
+let btnPluse = document.querySelector('.btn__pluse');
+let btnMines = document.querySelector('.btn__minus');
+let counter = document.querySelector('.result');
+
+let result = 0;
+counter.innerHTML = result;
+
+btnPluse.addEventListener('click', () => {
+  result += 1;
+  counter.innerHTML = result;
+});
+
+btnMines.addEventListener('click', () => {
+  result -= 1;
+  counter.innerHTML = result;
+});
+
