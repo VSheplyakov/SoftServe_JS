80de1e514645e7e12e3bbb60b13465f838b1259e
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-07-27 18:11:36 +0300

    edite

--- 9 lesson/born_idetify.js
+++ 9 lesson/born_idetify.js
@@ -11,18 +11,18 @@
 // Рядок - рядок цифр, що відповідає за ідентифікаційний код
 // Вихідні дані: Рядок, що описує, чи підходять введені дані під опис.
 
-// function identify(passport, phoneNumber, pin) {
-//   passport.length === 8 &&
-//   /[a-z]/gi.includes(passport[0][1]) &&
-//   /\d/g.test(passport.substring(2, 6))
-//     ? console.log(passport)
-//     : console.log('Passsport data uncorrect');
-//   phoneNumber.length === 12 && /[0-9]*/g.test(phoneNumber)
-//     ? console.log(phoneNumber)
-//     : console.log('Phone number data uncorrect');
-//   pin.length === 10 && /\d/g.test(pin)
-//     ? console.log(pin)
-//     : console.log('Pin data uncorrect');
-// }
+function identify(passport, phoneNumber, pin) {
+  passport.length === 8 &&
+  /[a-z]{2}[0-9]{6}/gi.test(passport)
+    ? console.log(passport)
+    : console.log('Passsport data uncorrect');
+  phoneNumber.length === 12 && /[0-9]{12}/g.test(phoneNumber)
+    ? console.log(phoneNumber)
+    : console.log('Phone number data uncorrect');
+  pin.length === 10 && /[0-9]{10}/g.test(pin)
+    ? console.log(pin)
+    : console.log('Pin data uncorrect');
+}
 
-// identify('AB123456', '380673606010', '1234567890');
+identify('AA123456', '380673606010', '1234567890');
+identify('A1123456', 'a80673606010', 'a234567890');
