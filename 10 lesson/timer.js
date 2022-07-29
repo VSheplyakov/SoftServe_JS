5e35e7fed7ff57449e75ab079179f3a1281b9689
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-07-29 14:20:06 +0300

    added js

--- 10 lesson/timer.js
+++ 10 lesson/timer.js
@@ -1,9 +1,13 @@
-timerCount = document.querySelector('.timer__box-timer')
-btnStart = document.querySelector('.btn__start')
-btnStop = document.querySelector('.btn__stop')
-btnCircle = document.querySelector('.btn__circle')
-btnPause = document.querySelector('.btn__pause')
-circleValue = document.querySelector('.timer__box-circle-value')
+const DOM = {
+  timerCount : document.querySelector('.timer__box-timer'),
+  btnStart : document.querySelector('.btn__start'),
+  btnStop : document.querySelector('.btn__stop'),
+  btnCircle : document.querySelector('.btn__circle'),
+  btnPause : document.querySelector('.btn__pause'),
+  circleValue : document.querySelector('.timer__box-circle-value'),
+};
+
+
 
 // Click Start - timer run
 // click stop - timer stop and clear timer and clear circles
